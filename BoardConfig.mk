5e8f4ccfd8e595abb1987f373ecfbf1676cefe1a
Kuba Wojciechowski <nullbytepl@gmail.com>
2021-05-05 19:34:08 +0800

    RMX1921: Disable kpti
    It can also help increase performance by a lot in some scenarios.
    
    Signed-off-by: Kuba Wojciechowski <nullbytepl@gmail.com>
    Change-Id: Ica465f586a44129047c266597316045fab762550
    Signed-off-by: Parixshit <parikshit.tunlr@slmail.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -42,6 +42,7 @@ BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a600000.dwc3
 BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += loop.max_part=7
+BOARD_KERNEL_CMDLINE += kpti=off
 BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
