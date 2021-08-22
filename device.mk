dfffc7c12097291c24c5a4142f726bb292d6b0f0
Mathieu Chartier <mathieuc@google.com>
2021-08-22 16:32:42 +0800

    RMX1921: Always preopt extracted APKs
    Change-Id: I2e4742a796b8142f14eaad5eba81eaf800d64914
    Signed-off-by: Parixshit <parikshit.tunlr@slmail.me>

--- device.mk
+++ device.mk
@@ -128,6 +128,7 @@ PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := everything
 
 # Speed profile services and wifi-service to reduce RAM and storage
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
 
 PRODUCT_DEXPREOPT_SPEED_APPS += \
     Launcher3QuickStep \
