29a6cf139a80d65937bc8e797555c7c1cdb4cbb4
Jorim Jaggi <jjaggi@google.com>
2022-01-19 02:57:20 +0530

    RMX1921: Disable sdm rotator downscaler Rotator down scaler is causing additional delays that are better handled by GL composition.
    Test: Open app, no jank
    Bug: 73640835
    Change-Id: I6ea9baa28acc222afcf365f3bf107149bc8974d9
    Signed-off-by: Parixshit <parikshit.tunlr@slmail.me>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -103,6 +103,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.display.enable_default_color_mode=1 \
+    vendor.display.disable_rotator_downscale=1 \
     ro.vendor.display.cabl=2 \
     ro.sf.lcd_density=402 \
     vendor.display.lcd_density=402
