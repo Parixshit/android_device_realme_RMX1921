cb34cc2cf3f6067c11a173750b55fa2aeb694383
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-01-19 03:20:34 +0530

    RMX1921: Enable Power HAL interaction boost
    Signed-off-by: Parixshit <parikshit.tunlr@slmail.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -150,6 +150,7 @@ BOARD_USES_METADATA_PARTITION := true
 
 # Power
 TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
+TARGET_USES_INTERACTION_BOOST := true
 
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
