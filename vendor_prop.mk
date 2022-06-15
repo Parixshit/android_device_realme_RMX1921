10cc25aa2f22493d9ca15993d244f216f9486fde
Andrzej Perczak <linux@andrzejperczak.com>
2022-06-15 06:35:54 +0000

    RMX1921: Disable partial FB excl rect
    Reference: https://source.codeaurora.org/quic/la/platform/hardware/qcom/display/commit/config/display-product.mk?h=LA.UM.9.1.r1-10900-SMxxx0.0&id=d22305d69c0fad06e6c7f166c9a86cc35d0ed42c
    Signed-off-by: Parixshit <parikshit.tunlr@slmail.me>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -105,6 +105,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     vendor.display.enable_default_color_mode=1 \
     vendor.display.disable_rotator_downscale=1 \
+    vendor.display.disable_excl_rect_partial_fb=1 \
     ro.vendor.display.cabl=2 \
     ro.sf.lcd_density=402 \
     vendor.display.lcd_density=402
