f44125120139c8975c93a5666801422d3184b949
Parixshit <parikshit.tunlr@slmail.me>
2022-07-07 03:08:21 +0530

    RMX1921: Update vendorsetup.sh Dialer APK - https://github.com/Parixshit/assets/blob/main/github.com_parixshit_android_packages_apps_Dialer.apk
    Signed-off-by: Parixshit <parikshit.tunlr@slmail.me>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,10 +2,6 @@
 KERNEL="kernel/realme/RMX1921"
 if [ ! -d "$KERNEL" ]; then
 git clone --depth=1 https://github.com/kaderbava/android_kernel_realme_RMX1921.git -b 11 kernel/realme/RMX1921
-cd kernel/realme/RMX1921
-git fetch https://github.com/Anujbarwal/android_kernel_realme_RMX1921.git
-git cherry-pick 81c6878d57e350c775b90b9a5ee51e062e3340da
-cd ..//..//..
 else
   echo "No Need: ${KERNEL} is already available there."
 fi
@@ -18,10 +14,10 @@ else
   echo "No Need: ${VENDOR} is already available there."
 fi
 
-# CLANG NEUTRON
+# CLANG NEUTRON-15
 NEUTRON="prebuilts/clang/host/linux-x86/clang-neutron"
 if [ ! -d "$NEUTRON" ]; then
-git clone --depth=1 https://github.com/Neutron-Toolchains/neutron-clang.git prebuilts/clang/host/linux-x86/clang-neutron
+git clone --depth=1 https://gitlab.com/dakkshesh07/neutron-clang.git -b Neutron-15 prebuilts/clang/host/linux-x86/clang-neutron
 else
   echo "No Need: ${NEUTRON} is already available there."
 fi
@@ -33,3 +29,7 @@ git clone --depth=1 https://github.com/HyperTeam/android_packages_apps_RealmeDir
 else
   echo "No Need: ${DIRAC} is already available there."
 fi
+
+# OOS DIALER
+rm -rf packages/apps/Dialer
+git clone --depth=1 https://github.com/Parixshit/android_packages_apps_Dialer.git packages/apps/Dialer
